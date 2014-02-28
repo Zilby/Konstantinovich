118226c835a5e6ebd5998263f54c283a7b41c99b
zilby <azilby@gmail.com>
2014-02-28 15:16:07 -0500

    finished sort

--- Sort/Sort.java
+++ Sort/Sort.java
@@ -3,31 +3,42 @@ import java.util.*;
 
 public class Sort{
 
-    public static int[] a = {1,1,2,4,5,5,7,8,9,9,9,11,11,13,14,14,15};
-    public static int[] b = {2,3,3,5,5,5,7,7,8,12,15,17,17,18};
+    public static int[] testa = {1,1,2,4,5,5,7,8,9,9,9,11,11,13,14,14,15};
+    public static int[] testb = {2,3,3,5,5,5,7,7,8,12,15,17,17,18};
 
     public static int[] merge(int[] a, int[] b){
-	int[] output = new int[(a.length)*(b.length)];
-	int counter = 0;
-	if(a.length>b.length){
-	    int[]  = a;
-	else
-	    int[]c = b;
-	for (int i = 0;i<c.length;i++){
-	    if(a[i]>b[i]){
-		output[counter] = a[i];
-		output[counter+1] = b[i];
-	    }else{
-		output[counter] = b[i];
-		output[counter+1] = a[i];
+	int[] output = new int[(a.length)+(b.length)];
+	int aCount = 0;
+	int bCount = 0;
+	for(int i=0;i<output.length;i++){
+	    if(aCount == a.length){
+		output[i]=b[bCount];
+		bCount++;
+	    } else if(bCount == b.length){
+		output[i]=a[aCount];
+		aCount++;
+	    } else {
+		if(a[aCount]<b[bCount]){
+		    output[i]=a[aCount];
+		    aCount++;
+		}else{
+		    output[i]=b[bCount];
+		    bCount++;
+		}
 	    }
-	    counter += 2;
-	    System.out.println(counter);
 	}
 	return output;
     }
 
+    public static void printArray(int[] output){
+	String stringout = "";
+	for(int j=0;j<output.length;j++){
+	    stringout += output[j] + " ";
+	}
+	System.out.println(stringout);
+    }
+
     public static void main(String[] args) {
-	System.out.println(merge(a,b));
+	printArray(merge(testa,testb));
     }
 }
